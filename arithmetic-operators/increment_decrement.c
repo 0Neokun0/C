7a14d5ae1da19e219f00489a8d668126dc40fb8f
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-23 19:10:22 +0900

    Increment And Decrement

---
+++ arithmetic-operators/increment_decrement.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num1 = 10, num2 = 100;
+    float num3 = 10.5, num4 = 100.5;
+
+    printf("++num1 was 10 now %d \n", ++num1);
+    printf("--num2 was 100 now %d \n", --num2);
+    printf("++num3 was 10.5 now %d \n", ++num3);
+    printf("--num4 was 100.5 now %d \n", --num4);
+
+    return 0;
+}
