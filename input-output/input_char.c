a4eaa662abd9c843a063593932702ba02b54eb3d
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-23 18:04:07 +0900

    Input Character

---
+++ input-output/input_char.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+    char character;
+    printf("Enter a character : ");
+    scanf("%c", &character);
+    printf("You entered %c. ", character);
+    return 0;
+}
