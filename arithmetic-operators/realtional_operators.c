47781dcfd0ea4c43e21024e06335c7bf3cee476f
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-24 14:09:01 +0900

    Relational Operators

---
+++ arithmetic-operators/realtional_operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  int num1 = 5, num2 = 5, num3 = 10;
+  
+    printf("%d  ==  %d is %d \n", num1, num3, num1 == num3);
+    printf("%d  ==  %d is %d \n", num1, num3, num1 == num3);
+    printf("%d  >   %d is %d \n", num1, num3, num1 > num3);
+    printf("%d  >   %d is %d \n", num1, num3, num1 > num3);
+    printf("%d  <   %d is %d \n", num1, num3, num1 < num3);
+    printf("%d  <   %d is %d \n", num1, num3, num1 < num3);
+    printf("%d  !=  %d is %d \n", num1, num3, num1 != num3);
+    printf("%d  !=  %d is %d \n", num1, num3, num1 != num3);
+    printf("%d  >=  %d is %d \n", num1, num3, num1 >= num3);
+    printf("%d  >=  %d is %d \n", num1, num3, num1 >= num3);
+    printf("%d  <=  %d is %d \n", num1, num3, num1 <= num3);
+    printf("%d  <=  %d is %d \n", num1, num3, num1 <= num3);
+  return 0;
+}
