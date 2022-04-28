06fa2ea3b278f20c557eeca69082892b642911ed
Your Name0Neokun0 <you@example.com>
2022-04-28 14:34:43 +0900

    Register Variable Defination

---
+++ Storage-class/register_variable.c
@@ -0,0 +1,6 @@
+/*
+The 'register' keyword is used to declare register variable.
+Register variables were supposed to be fater than local variables.
+Unless you are working on embedded systems where you know how to optimize
+code for the given application, there is no use of register variables.
+*/
