6516219760e8d8b42f886de13d40d47fec20a498
0Neokun0 <nishant.meher.9955@gmail.com>
2023-06-22 14:42:49 +0900

    pointers and array

---
+++ c_programming/c_pointer_and_character_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char C[4];
+    C[0] = 'N';
+    C[1] = 'I';
+    C[2] = 'S';
+    C[3] = 'H';
+    C[4] = '\0';
+    printf("%s", C);
+}
