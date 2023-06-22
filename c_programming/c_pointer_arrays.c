2e1e03e404400a0188f6fbb981fbc530a7229c64
0Neokun0 <nishant.meher.9955@gmail.com>
2023-06-22 13:28:11 +0900

    pointer and arrays

--- c_programming/c_pointer_arrays.c
+++ c_programming/c_pointer_arrays.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int a = 1025;
-    int *p;
-    p = &a;
+    int A[] ={2,4,5,8,1};
+    int i;
+    for(i = 0; i < 5; i++)
+    {
+        printf("Address = %d\n", &A[i]);
+        printf("Address = %d\n", A+i);
+        printf("value = %d\n", A[i]);
+        printf("value = %d\n", *(A+i));
+    }
     
 }
